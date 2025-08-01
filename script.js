cc131b3b4ac8c6fc17bfa29a6e34c227eb3bf19d
MiGaNg <miguelangelyv1@gmail.com>
2025-08-01 14:25:22 -0500

    Avance JS Proyecto

--- script.js
+++ script.js
@@ -11,6 +11,59 @@ let cid = [
   ['ONE HUNDRED', 100]
 ];
 
-// let {value} = document.getElementById('cash');
+const money = document.getElementById('money-span');
+money.innerHTML = cid.map(e=>{
+  const [name, amount] = e;
+  return `${name}: $${amount}<br>`;
+}).join('')
 
-// console.log("Cash: " + value);
+let cashInput = document.getElementById('cash');
+
+
+const spanTotal = document.getElementById('total');
+spanTotal.textContent = `Total: $ ${price}`;
+
+// const checkCash = () => {
+//   if( cashInput.value === '') {
+//     alert('Por favor, ingrese el dinero del cliente');
+//     return;
+//   }
+
+//   if(cashInput.value < price) {
+//     alert('El dinero ingresado es insuficiente');
+//     return;
+//   }
+// }
+
+const purchaseBtn = document.getElementById('purchase-btn');
+
+
+
+const showDue = () => {
+  // checkCash();
+  toggleChangeDueVisibility();
+  const changeDue = document.getElementById('change-due');
+  console.log(price, cashInput.value);
+  if(cashInput.value == price) {
+    changeDue.textContent = 'No hay cambio';
+
+  }else{
+    changeDue.textContent = 'Si hay cambio';
+  }
+
+  if(cashInput.value === '') {
+    changeDue.textContent = '';
+  }
+
+}
+
+const toggleChangeDueVisibility = () => {
+  const changeDue = document.getElementById('change-due');
+  if (changeDue.textContent.trim() === '') {
+    changeDue.style.display = 'none';
+  } else {
+    changeDue.style.display = 'inline';
+  }
+};
+
+purchaseBtn.addEventListener('click',showDue);
